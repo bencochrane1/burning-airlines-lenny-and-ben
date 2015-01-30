e1e1f6987b04e6105905a50dbd607867f43a1cb0
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-01-31 10:00:46 +1100

    Created controller actions index and create for planes

--- app/controllers/planes_controller.rb
+++ app/controllers/planes_controller.rb
@@ -1,2 +1,18 @@
 class PlanesController < ApplicationController
+
+  def index
+    render json: Plane.all
+  end
+
+  def create
+    @plane = Plane.create(plane_params)
+    render json: @plane
+  end
+
+private
+
+  def plane_params
+    params.require(:plane).permit(:name, :rows, :aisles)
+  end
+
 end
