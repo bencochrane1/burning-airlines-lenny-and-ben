4c78cd108ad3cb6e72741b8f89f966649dd410c3
Lenny Boyatzis <lennyboyatzis@gmail.com>
2015-01-31 14:12:54 +1100

    Commit before adding flights model

--- app/assets/javascripts/views/planesView.js
+++ app/assets/javascripts/views/planesView.js
@@ -39,14 +39,48 @@ App.PlanesView = Backbone.View.extend ({
     var aisles = this.$el.find('.aisles').val();
 
     var newPlane = this.collection.create({ name: name, rows: rows, aisles: aisles});
-    
-    console.log(newPlane);
-    console.log(newPlane.toJSON());
-
+   
   },
 
-  appendNewPlane: function() {
-    console.log("you're out!");
-  }
+  appendNewPlane: function(plane) {
+    console.log(plane);
+    var planeView = new App.PlaneView({ model: plane })
+    this.$el.append(planeView.render().el);
+    this.createTable();
+  },
+
+  createTable: function() {
+ 
+    var tableElem, rowElem, colElem;
+
+    var a = this.$el.find('.rows').val();
+    var b = this.$el.find('.aisles').val();
+   
+
+   if (a == "" || b == "") {
+     alert("Please enter some numeric value");
+   } else {
+     tableElem = document.createElement('table');
+
+     for (var i = 0; i < a; i++) {
+       rowElem = document.createElement('tr');
+
+       for (var j = 0; j < b; j++) {
+         colElem = document.createElement('td');
+         colElem.appendChild(document.createTextNode(j + 1)); //to print cell number
+         rowElem.appendChild(colElem);
+       }
+
+       tableElem.appendChild(rowElem);
+     }
+
+     document.body.appendChild(tableElem);
+
+
+   }
+ }
+
+
+
 
 });
