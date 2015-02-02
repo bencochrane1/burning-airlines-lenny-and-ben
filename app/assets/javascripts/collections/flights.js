67770c18cb6ab405bf20ba25ae39ed3f526bd592
Ben Cochrane <bencochrane1@me.com>
2015-02-02 11:27:02 +1100

    fix create flight link

--- app/assets/javascripts/collections/flights.js
+++ app/assets/javascripts/collections/flights.js
@@ -11,8 +11,6 @@ App.Flights = Backbone.Collection.extend({
         });
 
         savedSearch = matchedFlights;
-
-        // matchedOriginFlights = [];
           
         matchedOriginFlights = new App.Flights(savedSearch);
         console.log(matchedOriginFlights);
@@ -21,36 +19,7 @@ App.Flights = Backbone.Collection.extend({
             return matchedOriginFlights.attributes.destination.toLowerCase().indexOf(searchDestination.toLowerCase()) !== -1;
         });
 
-        // return  matchedRestOfFlights;
-
         return new App.Flights(matchedRestOfFlights);
-
-
   }
 
-
-
-
 });
-
-
-
-
-
-
-
-// var App = App || {}
-
-// App.Flights = Backbone.Collection.extend({
-//   url: '/flights',
-//   model: App.Flight,
-
-//   filterBySearch: function (searchOrigin) {
-//     var matchedFlights = this.filter(function(flight){
-
-//       return flight.attributes.origin.toLowerCase().indexOf(searchOrigin.toLowerCase()) !== -1;
-//       // return flight.attributes.destination.indexOf(searchDestination) !== -1;
-//     });
-//       return new App.Flights(matchedFlights);
-//   }
-// });
